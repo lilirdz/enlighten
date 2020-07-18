3312fd8a4682fc0b4c024ead6353c5210b560636
lilirdz <lilimrod@gmail.com>
2020-07-18 15:26:08 -0500

    still working on backend

--- backend/app/controllers/api/v1/school_years_controller.rb
+++ backend/app/controllers/api/v1/school_years_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::SchoolYearsController < ApplicationController
+
+    def index
+        render json: SchoolYear.all
+    end
+
+    def show
+      render json: SchoolYear.find(params[:id])
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :comments
-      resources :school_years
+      resources :school_years, only: [:index,:show]
       resources :articles, only: [:index,:show]
       resources :prospective_schools
       resources :schools, only: [:index,:show]
